e8a1144aeaaab0b5a1aca20467e7a513f805e842
Josip Kilić <neksuses@gmail.com>
2023-11-05 02:04:12 +0100

    `WeatherError` done

--- lib/screens/weather/widgets/weather/weather_error.dart
+++ lib/screens/weather/widgets/weather/weather_error.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:intl/intl.dart';
 
+import '../../../../constants/colors.dart';
+import '../../../../constants/durations.dart';
+import '../../../../constants/icons.dart';
+import '../../../../constants/text_styles.dart';
 import '../../../../models/location/location.dart';
+import '../../../../util/color.dart';
 
-// TODO: Finish this
 class WeatherError extends StatelessWidget {
   final Location location;
   final String error;
@@ -13,5 +19,104 @@ class WeatherError extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) => Container(
+        width: MediaQuery.sizeOf(context).width,
+        margin: const EdgeInsets.only(bottom: 24),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.vertical(
+            bottom: Radius.circular(40),
+          ),
+          gradient: LinearGradient(
+            colors: [
+              lightenColor(PromajaColors.red),
+              darkenColor(PromajaColors.red),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: Animate(
+          onPlay: (controller) => controller.loop(
+            count: 7,
+            reverse: true,
+          ),
+          effects: [
+            FadeEffect(
+              curve: Curves.easeIn,
+              duration: PromajaDurations.errorFadeAnimation,
+            ),
+          ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox.shrink(),
+
+              ///
+              /// DATE & LOCATION
+              ///
+              Column(
+                children: [
+                  const SizedBox(height: 24),
+                  Text(
+                    DateFormat.yMMMMd().format(DateTime.now()),
+                    style: PromajaTextStyles.weatherCardLastUpdated,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    location.name,
+                    style: PromajaTextStyles.currentLocation,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+
+              ///
+              /// ERROR ICON
+              ///
+              Transform.scale(
+                scale: 1.2,
+                child: Image.asset(
+                  PromajaIcons.tornado,
+                  height: 176,
+                  width: 176,
+                ),
+              ),
+
+              ///
+              /// ERROR
+              ///
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 80),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Error',
+                      style: PromajaTextStyles.errorFetching,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      error,
+                      style: PromajaTextStyles.currentWeather,
+                      textAlign: TextAlign.center,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+
+              ///
+              /// ADDITIONAL INFO
+              ///
+              Container(
+                height: 64,
+                color: Colors.transparent,
+                margin: const EdgeInsets.all(8),
+              ),
+            ],
+          ),
+        ),
+      );
 }
