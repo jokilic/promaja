5307770cf36a96c69a83a355b23dee327cc5cdb2
Josip Kilić <neksuses@gmail.com>
2023-11-05 01:59:10 +0100

    `WeatherLoading` done

--- lib/screens/weather/widgets/weather/weather_loading.dart
+++ lib/screens/weather/widgets/weather/weather_loading.dart
@@ -1,8 +1,10 @@
+import 'package:animated_shimmer/animated_shimmer.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../constants/colors.dart';
 import '../../../../models/location/location.dart';
+import '../../../../util/color.dart';
 
-// TODO: Finish this
 class WeatherLoading extends StatelessWidget {
   final Location location;
 
@@ -11,5 +13,93 @@ class WeatherLoading extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) => Container(
+        width: MediaQuery.sizeOf(context).width,
+        margin: const EdgeInsets.only(bottom: 24),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.vertical(
+            bottom: Radius.circular(40),
+          ),
+          gradient: LinearGradient(
+            colors: [
+              lightenColor(Colors.amberAccent),
+              darkenColor(Colors.amberAccent),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ...List.generate(10, (index) => const SizedBox.shrink()),
+
+            ///
+            /// LAST UPDATED & LOCATION
+            ///
+            Column(
+              children: [
+                AnimatedShimmer(
+                  height: 16,
+                  width: 104,
+                  startColor: PromajaColors.white.withOpacity(0.5),
+                  endColor: PromajaColors.white.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                const SizedBox(height: 16),
+                AnimatedShimmer(
+                  height: 32,
+                  width: 200,
+                  startColor: PromajaColors.white.withOpacity(0.5),
+                  endColor: PromajaColors.white.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ],
+            ),
+
+            ...List.generate(5, (index) => const SizedBox.shrink()),
+
+            ///
+            /// WEATHER ICON
+            ///
+            AnimatedShimmer.round(
+              size: 176,
+              startColor: PromajaColors.white.withOpacity(0.5),
+              endColor: PromajaColors.white.withOpacity(0.25),
+            ),
+
+            ...List.generate(5, (index) => const SizedBox.shrink()),
+
+            ///
+            /// TEMPERATURE & WEATHER
+            ///
+            Column(
+              children: [
+                AnimatedShimmer(
+                  width: 144,
+                  height: 104,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                const SizedBox(height: 16),
+                AnimatedShimmer(
+                  height: 32,
+                  width: 200,
+                  startColor: PromajaColors.white.withOpacity(0.5),
+                  endColor: PromajaColors.white.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ],
+            ),
+
+            ///
+            /// ADDITIONAL INFO
+            ///
+            Container(
+              height: 144,
+              color: Colors.transparent,
+              margin: const EdgeInsets.all(8),
+            ),
+          ],
+        ),
+      );
 }
