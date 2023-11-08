1d33fe98b42b6ff44415247e1926e91fcdfd36cb
Josip Kilić <neksuses@gmail.com>
2023-11-09 00:36:03 +0100

    Flutter work regarding work manager & home widget

---
+++ lib/services/home_widget_service.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_widget/home_widget.dart';
+
+import 'logger_service.dart';
+
+///
+/// Service which initializes `HomeWidget`
+/// Used for showing widgets in the homescreens of Android & iOS
+///
+
+final homeWidgetProvider = Provider<HomeWidgetService>(
+  (_) => throw UnimplementedError(),
+  name: 'HomeWidgetProvider',
+);
+
+class HomeWidgetService {
+  ///
+  /// CONSTRUCTOR
+  ///
+
+  final LoggerService logger;
+
+  HomeWidgetService(this.logger)
+
+  ///
+  /// INIT
+  ///
+
+  {
+    HomeWidget.setAppGroupId('group.com.josipkilic.promaja');
+  }
+
+  ///
+  /// METHODS
+  ///
+
+  /// Renders new `HomeWidget` & updates it
+  Future<void> createHomeWidget(Widget widget) async {
+    await renderWidget(widget);
+    await updateWidget();
+  }
+
+  /// Renders a Flutter widget  as a `HomeWidget`
+  Future<void> renderWidget(Widget widget) async {
+    try {
+      await HomeWidget.renderFlutterWidget(
+        widget,
+        key: 'widgetFile',
+      );
+    } catch (e) {
+      final error = 'renderWidget - $e';
+      logger.e(error);
+    }
+  }
+
+  /// Updates `HomeWidget`
+  Future<void> updateWidget() async {
+    try {
+      await HomeWidget.updateWidget(
+        name: 'WidgetView',
+        androidName: 'WidgetView',
+        iOSName: 'PromajaWidget',
+        qualifiedAndroidName: 'com.josipkilic.promaja.WidgetView',
+      );
+    } catch (e) {
+      final error = 'updateWidget - $e';
+      logger.e(error);
+    }
+  }
+}
--- lib/services/work_manager_service.dart
+++ lib/services/work_manager_service.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:workmanager/workmanager.dart';
 
+import 'api_service.dart';
+import 'dio_service.dart';
+import 'hive_service.dart';
+import 'home_widget_service.dart';
 import 'logger_service.dart';
 
 ///
@@ -19,9 +24,9 @@ class WorkManagerService {
   /// CONSTRUCTOR
   ///
 
-  final LoggerService loggerService;
+  final LoggerService logger;
 
-  WorkManagerService(this.loggerService)
+  WorkManagerService(this.logger)
 
   ///
   /// INIT
@@ -40,9 +45,44 @@ class WorkManagerService {
 }
 
 @pragma('vm:entry-point')
-void callbackDispatcher() => Workmanager().executeTask(
-      (task, inputData) {
-        try {} catch (e) {}
-        return Future.value(true);
+Future<void> callbackDispatcher() async => Workmanager().executeTask(
+      (task, inputData) async {
+        try {
+          /// Initialize services
+          final logger = LoggerService();
+          final dio = DioService(logger);
+          final api = APIService(
+            logger: logger,
+            dio: dio.dio,
+          );
+          final homeWidget = HomeWidgetService(logger);
+          final hive = HiveService(logger);
+          await hive.init();
+
+          /// Get location to fetch
+          final location = hive.getLocationsFromBox().first;
+
+          /// Fetch weather data for location
+          final response = await api.getForecastWeather(
+            query: '${location.lat},${location.lon}',
+            days: 3,
+          );
+
+          /// Response is successful
+          if (response.response != null && response.error == null) {
+            return Future.value(true);
+          }
+
+          /// There was an error fetching weather
+          else {
+            final error = 'callbackDispatcher -> error fetching weather -> ${response.error}';
+            Logger().e(error);
+            return Future.value(false);
+          }
+        } catch (e) {
+          final error = 'callbackDispatcher - $e';
+          Logger().e(error);
+          return Future.error(error);
+        }
       },
     );
