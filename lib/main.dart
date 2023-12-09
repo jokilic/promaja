6241860eb3e7e0f7f9909897ba3e2911f2f56bd3
Josip Kilić <neksuses@gmail.com>
2023-12-09 17:38:33 +0100

    Remove testing from `main`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:background_fetch/background_fetch.dart';
+import 'dart:ui';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,20 +8,16 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:stack_trace/stack_trace.dart';
 
 import 'constants/colors.dart';
+import 'services/background_fetch_service.dart';
 import 'services/dio_service.dart';
 import 'services/hive_service.dart';
 import 'services/home_widget_service.dart';
 import 'services/logger_service.dart';
-import 'services/background_fetch_service.dart';
 import 'widgets/promaja_navigation_bar.dart';
 
 Future<void> main() async {
-  await backgroundFetchHeadlessTask(
-    HeadlessTask('com.transistorsoft.fetch', false),
-  );
-  return;
-
   WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
 
   /// Parsing of [StackTrace]
   FlutterError.demangleStackTrace = (stack) {
