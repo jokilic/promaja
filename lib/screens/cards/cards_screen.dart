a38a1d6d695a64072a4869a189d8368124cdb75f
Josip Kilić <neksuses@gmail.com>
2025-11-23 17:06:55 +0100

    Revert card swipe updates

--- lib/screens/cards/cards_screen.dart
+++ lib/screens/cards/cards_screen.dart
@@ -74,9 +74,6 @@ class CardsScreen extends ConsumerWidget {
               )
             else
               CardSwiper(
-                allowedSwipeDirection: const AllowedSwipeDirection.symmetric(
-                  horizontal: true,
-                ),
                 backCardOffset: const Offset(0, 48),
                 padding: EdgeInsets.only(
                   bottom: getCurrentCardBottomPadding(context),
--- lib/screens/weather/widgets/weather/weather_success.dart
+++ lib/screens/weather/widgets/weather/weather_success.dart
@@ -92,9 +92,6 @@ class _WeatherSuccessState extends ConsumerState<WeatherSuccess> {
           )
         else
           CardSwiper(
-            allowedSwipeDirection: const AllowedSwipeDirection.symmetric(
-              horizontal: true,
-            ),
             backCardOffset: const Offset(0, 48),
             padding: EdgeInsets.only(
               bottom: getWeatherCardBottomPadding(context),
