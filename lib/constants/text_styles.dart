3d1a6257a0c17fbc773ac37e19b845225d79e538
Josip Kilić <neksuses@gmail.com>
2026-05-26 21:11:08 +0200

    Thicker text

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -7,86 +7,98 @@ class PromajaTextStyles {
     fontFamily: 'ProductSans',
     color: PromajaColors.black,
     fontSize: 14,
+    fontWeight: FontWeight.w500,
   );
   static final searchResultSubtitle = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.black.withValues(alpha: 0.8),
     fontSize: 12,
+    fontWeight: FontWeight.w500,
   );
   static final searchTextFieldHint = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.black.withValues(alpha: 0.8),
     fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
   static const searchTextField = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.black,
     fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
   static const currentTemperature = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 88,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static final weatherTemperatureMin = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.5),
     fontSize: 64,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const weatherTemperatureMax = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 64,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static final weatherSummaryTemperatureMin = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.5),
     fontSize: 40,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const weatherSummaryTemperatureMax = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 40,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const currentHourTemperature = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 80,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const currentTemperatureDegrees = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 56,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const currentWeather = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 22,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static final currentLastUpdated = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.4),
     fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
   static const currentLocation = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 24,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const listLocation = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 20,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1,
   );
   static const listTemperature = TextStyle(
@@ -94,41 +106,44 @@ class PromajaTextStyles {
     color: PromajaColors.white,
     fontSize: 48,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const listErrorData = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const currentListData = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 11,
+    fontWeight: FontWeight.w500,
   );
   static final currentListDescription = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.4),
     fontSize: 11,
+    fontWeight: FontWeight.w500,
   );
   static const snackbar = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const noLocations = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 24,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1.35,
   );
   static const noLocationsPromaja = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 28,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
     height: 1.35,
   );
   static const errorFetching = TextStyle(
@@ -136,13 +151,13 @@ class PromajaTextStyles {
     color: PromajaColors.white,
     fontSize: 32,
     height: 1,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const weatherCardHourTemperature = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 20,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1,
   );
   static const weatherCardHourDegrees = TextStyle(
@@ -150,113 +165,119 @@ class PromajaTextStyles {
     color: PromajaColors.white,
     fontSize: 24,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static final weatherCardHourHour = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.8),
     fontSize: 12,
+    fontWeight: FontWeight.w500,
   );
   static final weatherCardLastUpdated = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.8),
     fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
   static const weatherCardHourChanceOfRain = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 12,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const weatherCardIndividualHourChanceOfRain = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 12,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const homeWidgetLocation = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 18,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const homeWidgetCurrentTemperature = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 40,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static final homeWidgetForecastTemperatureMin = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white.withValues(alpha: 0.5),
     fontSize: 32,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const homeWidgetForecastTemperatureMax = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 32,
     height: 1,
+    fontWeight: FontWeight.w500,
   );
   static const homeWidgetDescription = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const homeWidgetChanceOfRain = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 12,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const homeWidgetPromaja = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 14,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const settingsTitle = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 40,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const settingsSubtitle = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 24,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const settingsText = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
   static const settingsDialog = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 18,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1.45,
   );
   static const settingsDialogButton = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const settingsAbbreviation = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 22,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
   );
   static const settingsListTitle = TextStyle(
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 22,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1,
   );
   static const settingsListSubtitle = TextStyle(
@@ -270,7 +291,7 @@ class PromajaTextStyles {
     fontFamily: 'ProductSans',
     color: PromajaColors.white,
     fontSize: 18,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
     height: 1,
   );
 }
